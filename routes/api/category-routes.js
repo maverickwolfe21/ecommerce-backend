126c9c6c38216b080d8beb505ad1d260528a9b56
Maverick Wolfe <maverick.m.wolfe@gmail.com>
2024-02-07 18:33:02 -0700

    fixed issues with put requests

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,7 +50,7 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
   try {
-    const newCategory = await Category.update(req.body);
+    const newCategory = await Category.update(req.body, { where: { id: req.params.id } });
     res.status(200).json(newCategory);
   } catch (err) {
     res.status(400).json(err);
